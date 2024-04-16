6bc2003c3bf1383199160392c345d72c92b7f3ff
Sidor_md45@top-academy.ru <Sidor_md45@top-academy.ru>
2024-04-16 20:38:10 +0300

    HomeWork DONE

--- Arrays/main.cpp
+++ Arrays/main.cpp
@@ -12,27 +12,30 @@ void Print(const int arr[], const int n);
 void Sort(int arr[], const int n);
 int Sum(const int arr[], const int n);
 double Avg(const int arr[], const int n);
-void minValueIn(const int arr[], const int n);
-void maxValueIn(const int arr[], const int n);
-void ShiftLeft(int arr[], const int n);
-void ShiftRight(int arr[], const int n);
+int minValueIn(const int arr[], const int n);
+int maxValueIn(const int arr[], const int n);
+void ShiftLeft(int arr[], const int n, int num_of_shifts);
+void ShiftRight(int arr[], const int n, int num_of_shifts);
 
 void main()
 {
 	setlocale(LC_ALL, "");
 	const int n = 5;
 	int arr[n];
+	int num_of_shifts;
 	FillRand(arr, n);
 	Print(arr, n);
 	Sort(arr, n);
 	Print(arr, n);
 	cout << "Сумма элементов массива: " << Sum(arr, n) << endl;
 	cout << "Среднее арифмитическое элементов массива " << Avg(arr, n) << endl;
-	minValueIn(arr, n);
-	maxValueIn(arr, n);
-	ShiftLeft(arr, n);
+	cout << "Мнимальное значение массива: " << minValueIn(arr, n) << endl;
+	cout << "Максимальное значения массива: "<< maxValueIn(arr, n) << endl;	
+	cout << "Введите количество сдвигов влево: "; cin >> num_of_shifts;
+	ShiftLeft(arr, n, num_of_shifts);
 	Print(arr, n);
-	ShiftRight(arr, n);
+	cout << "Введите количество сдвигов вправо: "; cin >> num_of_shifts;
+	ShiftRight(arr, n, num_of_shifts);
 	Print(arr, n);
 }
 
@@ -76,32 +79,28 @@ int Sum(const int arr[], const int n)
 }
 double Avg(const int arr[], const int n)
 {
-	double Avg = 0;
-	Avg = (double)Sum(arr, n) / n;
-	return Avg;
+	return (double)Sum(arr, n) / n;
 }
-void minValueIn(const int arr[], const int n)
+int minValueIn(const int arr[], const int n)
 {
 		int minValue = arr[0];
 	for (int i = 0; i < n; i++)
 	{
 		if (minValue > arr[i])minValue = arr[i];
 	}
-	cout << "Минимальное значение массива: " << minValue << endl;
+	return minValue;
 }
-void maxValueIn(const int arr[], const int n)
+int maxValueIn(const int arr[], const int n)
 {
 	int maxValue = arr[0];
 	for (int i = 0; i < n; i++) 
 	{
 		if (maxValue < arr[i])maxValue = arr[i];
 	}
-	cout << "Максимально значение массива: " << maxValue << endl;
+	return maxValue;
 }
-void ShiftLeft(int arr[], const int n) 
+void ShiftLeft(int arr[], const int n,int num_of_shifts)
 {
-	int num_of_shifts;
-	cout << "Введите количество сдвигов влево: "; cin >> num_of_shifts;
 	for (int i = 0; i < num_of_shifts; i++)
 	{
 	int buffer = arr[0];
@@ -113,17 +112,7 @@ void ShiftLeft(int arr[], const int n)
 
 	}
 }
-void ShiftRight(int arr[], const int n) 
+void ShiftRight(int arr[], const int n,int num_of_shifts) 
 {
-	int num_of_shifts;
-	cout << "Введите количество сдвигов вправо: "; cin >> num_of_shifts;
-	for (int i = 0; i<num_of_shifts; i++) 
-	{
-		int buffer = arr[n - 1];
-		for(int i = n - 1; i > 0; i--) 
-		{
-			arr[i] = arr[i - 1];
-		}
-		arr[0] = buffer;
-	}
+	ShiftLeft(arr, n, n - num_of_shifts);
 }
