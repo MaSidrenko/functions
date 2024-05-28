7dcf6edb3e2ac6a971d0e1e28045ddc53ae8e504
Sidor_md45@top-academy.ru <Sidor_md45@top-academy.ru>
2024-05-28 16:05:57 +0300

    Arrays lib test DONE

---
+++ include/Constants.h
@@ -0,0 +1,6 @@
+#pragma once
+#define tab "\t"
+#define delimiter "\n|----------------------------------------------------------------------------|\n" << endl
+
+const int ROWS = 3;
+const int COLS = 4;
---
+++ include/Shift.h
@@ -0,0 +1,12 @@
+#pragma once
+#include"Constants.h"
+void ShiftLeft(int arr[], const int n, int num_of_shifts);
+void ShiftLeft(double arr[], const int n, int num_of_shifts);
+void ShiftLeft(char arr[], const int n, int num_of_shifts);
+
+void ShiftLeft(int arr[ROWS][COLS], const int ROWS, const int COLS, int num_of_shifts);
+
+
+void ShiftRight(int arr[], const int n, int num_of_shifts);
+void ShiftRight(double arr[], const int n, int num_of_shifts);
+void ShiftRight(char arr[], const int n, int num_of_shifts);
